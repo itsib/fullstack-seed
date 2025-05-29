0d64daa0a3dd699cd693527bf54e841f702e4516
Sergey Krainov <itsib.su@gmail.com>
2025-05-29 23:22:51 +0700

    chec gpg

--- apps/front/src/utils/theme.ts
+++ apps/front/src/utils/theme.ts
@@ -5,10 +5,8 @@ export function getActiveTheme(theme: Theme): Exclude<Theme, 'system'> {
     return theme;
   }
 
-  if (typeof window !== 'undefined') {
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      return 'dark';
-    }
+  if (typeof window !== 'undefined' && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return 'dark';
   }
 
   return 'light';
